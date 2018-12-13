83128b8b93f116ccd773bd53a691cffce573ec9f
jalbr74 <jalbr74@gmail.com>
2018-12-13 13:53:01 -0700

    Minor tweaks

--- docs/src/app/pages/components-page/components/buttons-example/examples/icon-buttons.ts
+++ docs/src/app/pages/components-page/components/buttons-example/examples/icon-buttons.ts
@@ -3,13 +3,8 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-icon-buttons',
     templateUrl: './icon-buttons.html',
-    styles: ['button { margin: 0 5px 5px 0; } .results { margin-top: 20px; } ']
+    styles: ['.btn-container { margin-bottom: 10px; } .btn-container > .btn + .btn.btn-link {margin-left: 10px; }']
 })
 export class IconButtonsComponent {
-    timesButtonPressed = 0;
-
-    buttonPressed(): void {
-        this.timesButtonPressed++;
-    }
 }
 
