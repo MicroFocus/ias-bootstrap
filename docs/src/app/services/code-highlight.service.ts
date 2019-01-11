acb3a2bcbd7eee55cbd319640db77b3f0a882496
jalbr74 <jalbr74@gmail.com>
2019-01-11 11:17:52 -0700

    Added content to the Getting Started page.

--- docs/src/app/services/code-highlight.service.ts
+++ docs/src/app/services/code-highlight.service.ts
@@ -7,8 +7,8 @@ import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-scss';
-// import 'prismjs/components/prism-markup';
-// import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-json';
 
 
 // Prism tries to highlight the whole document on DOMContentLoad.
