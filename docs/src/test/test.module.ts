8d61abd742422484c62733ab70b96e54f55d2c8c
jalbr74 <jalbr74@gmail.com>
2019-02-26 10:22:51 -0700

    Fixed development turnaround speed problems when running backstop tests

--- docs/src/test/test.module.ts
+++ docs/src/test/test.module.ts
@@ -5,11 +5,23 @@ import { TestRoutingModule } from './test-routing.module';
 import { TestComponent } from './test.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AccordionModule, CheckboxModule } from '@ux-aspects/ux-aspects';
+import { AccordionBasicComponent } from '../app/pages/components-page/components/accordion-example/examples/accordion-basic';
+import { TextButtonsComponent } from '../app/pages/components-page/components/buttons-example/examples/text-buttons';
+import { IconButtonsComponent } from '../app/pages/components-page/components/buttons-example/examples/icon-buttons';
+import { IconTextButtonsComponent } from '../app/pages/components-page/components/buttons-example/examples/icon-text-buttons';
+import { CheckboxesComponent } from '../app/pages/components-page/components/form-field-example/examples/checkboxes';
+import { RadioButtonsComponent } from '../app/pages/components-page/components/form-field-example/examples/radio-buttons';
 
 @NgModule({
     declarations: [
         TestComponent,
-        LandingPageComponent
+        LandingPageComponent,
+        AccordionBasicComponent,
+        TextButtonsComponent,
+        IconButtonsComponent,
+        IconTextButtonsComponent,
+        CheckboxesComponent,
+        RadioButtonsComponent
     ],
     imports: [
         BrowserModule,
