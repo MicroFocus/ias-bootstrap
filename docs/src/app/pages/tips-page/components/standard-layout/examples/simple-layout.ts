74fc23b4843fbd7e96ee7a4ae32d39e256ece2ef
McKay Larsen <mckay.larsen@microfocus.com>
2019-04-30 15:26:47 -0600

    TIPS: Added simple-layout example

--- docs/src/app/pages/tips-page/components/standard-layout/examples/simple-layout.ts
+++ docs/src/app/pages/tips-page/components/standard-layout/examples/simple-layout.ts
@@ -3,7 +3,10 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-simple-layout',
     templateUrl: './simple-layout.html',
-    styleUrls: ['./simple-layout.html']
+    styleUrls: ['./simple-layout.scss']
 })
 export class SimpleLayoutComponent {
+    cancelButtonPressed(): void {}
+    closeButtonPressed(): void {}
+    saveButtonPressed(): void {}
 }
